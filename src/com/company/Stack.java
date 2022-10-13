fa65cf9d33df0e9eba6bdb6f2d6035fd94ba7c9e
annasimonyan1407 <annasimonyan1407@gmail.com>
2022-10-13 20:58:53 +0400

    Fifo

--- src/com/company/Stack.java
+++ src/com/company/Stack.java
@@ -24,17 +24,64 @@ public class Stack {
             return stck[tos--];
     }
 
+
+    // FIFO
+    public static class Fifostack{
+        int stack[];
+        int firstNumber;
+        int lastNumber;
+
+
+        Fifostack(int n){
+            stack = new int[n];
+            firstNumber = -1;
+            lastNumber =-1;
+        }
+
+        public void push(int item){
+            if(firstNumber == stack.length-1){
+                System.out.println("stack is full");
+            }
+            else
+               stack[++firstNumber] = item;
+
+        }
+
+        public int pop(){
+            if(lastNumber==firstNumber)
+            {
+                System.out.println("stack is empty");
+                return 0;
+            }
+            else return stack[++lastNumber];
+        }
+
+    }
+
     public static void main(String[] args) {
 //        int [] tiv={1,6,8,7,47};
-        Stack stack = new Stack(2);
+        Stack stack = new Stack(6);
         stack.push(9);
         stack.push(15);
         stack.push(2);
         System.out.println(stack.pop());
         System.out.println(stack.pop());
         System.out.println(stack.pop());
+        System.out.println(stack.pop());
 
 //        stack.pop();
+
+        Fifostack fifostack = new Fifostack(4);
+        fifostack.push(3);
+        fifostack.push(4);
+        fifostack.push(5);
+        fifostack.push(6);
+
+        System.out.println(fifostack.pop());
+        System.out.println(fifostack.pop());
+        System.out.println(fifostack.pop());
+        System.out.println(fifostack.pop());
+
 
 
     }
